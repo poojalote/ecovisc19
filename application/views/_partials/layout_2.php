2475abbe6826dbf782cd04ece72daef0c0698877
AvinashGbtech <92419904+AvinashGbtech@users.noreply.github.com>
2022-01-15 12:32:54 +0530

    by pooja

--- application/views/_partials/layout_2.php
+++ application/views/_partials/layout_2.php
@@ -4,6 +4,8 @@ $username = "";
 if (isset($this->session->user_session)) {
 	$username = $this->session->user_session->name;
 	$role = $this->session->user_session->roles;
+	$type = $this->session->user_session->user_type;
+	$default_access = $this->session->user_session->default_access;
 } else {
 	redirect("");
 }
@@ -23,6 +25,9 @@ if (isset($this->session->user_session)) {
 			height: 50px;
 		}
 	}
+    body.layout-2 .main-wrapper{
+        padding: 0 25px;
+    }
 
 
 </style>
@@ -33,10 +38,55 @@ if (isset($this->session->user_session)) {
 	<div class="main-wrapper container-fluid">
 		<div class="navbar-bg"  style="background: #891635;"></div>
 		<!-- nav bar start -->
+		<?php
+		if ($role == 1) {
+			$url = base_url()."admin/dashboard";
+		} else {
+			if($role == 2) {
+				if ($type == 5) {
+					$url = base_url(). "hospitalMedicine";
+					// $url = base_url(). "pharmeasy";
+				} else {
+					if ($default_access !== 2) {
+						$url = base_url(). "patient_info";
+					}else{
+						$url = base_url(). "icubedManagement";
+					}
+				}
+			}else{
+				if ($type ==4) {
+					$url = base_url(). "pharmeasy";
+					// $url = base_url(). "hospitalMedicine";
+				} else if ($type ==6) {
+					$url = base_url(). "view_pickup";
+					// $url = base_url(). "pharmeasy";
+				}
+				else if ($type ==12) {
+					$url = base_url(). "labpatient_info";
+				}
+				else{
+					$url = base_url(). "view_radiologypickup";
+				}
+
+			}
+		}
+		?>
 		<nav class="navbar navbar-expand-lg main-navbar">
 			<a href="#" class="nav-link nav-link-lg mobile_three" onclick="mobileMenuHideShoe()"></a>
-			<a href="<?php echo base_url(); ?>patient_info" class="navbar-brand sidebar-gone-hide">
-				<img src="<?php echo base_url(); ?>dist/img/credit/healthstart.jpeg" id="site-logo"
+			<a href="<?php echo $url; ?>" class="navbar-brand sidebar-gone-hide">
+                <?php
+                if (!is_null($this->session->branch_logo)) {
+                    if(!is_null($this->session->branch_logo->branch_logo)){
+                        $logo = $this->session->branch_logo->branch_logo;
+                    }else{
+                        $logo = "dist/img/credit/healthstart.jpeg";
+                    }
+                } else {
+                    $logo = "dist/img/credit/healthstart.jpeg";
+
+                }
+                ?>
+				<img src="<?php echo base_url($logo); ?>" id="site-logo"
 					 class="logo_image rounded-circle">
 			</a>
 			<form class="form-inline ml-auto mobile_search">
